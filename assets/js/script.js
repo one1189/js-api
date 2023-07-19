0132cc964303a7d4a48776f1caee62d4e0517b31
Chris Watton <119423330+one1189@users.noreply.github.com>
2023-07-19 15:40:55 +0100

    add js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-const API_key = "L2ZEQkgNGuOc3A-GzSPT_J85Gx8"
+const API_key = "L2ZEQkgNGuOc3A-GzSPT_J85Gx8";
+const API_url = "https://ci-jshint.herokuapp.com/";
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+
+documents.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_url}?api_key=${API_key}`;
+
+    const response = await fetch(getStatus);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
